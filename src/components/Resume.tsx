8b50f637104872ec2870b319e61ad5680859e236
resume-builder <princewelmanbhat22@gmail.com>
2024-12-04 12:56:15 +0530

    changes

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -13,28 +13,89 @@ const Resume: React.FC<{ data: ResumeData }> = ({ data }) => {
   } = data;
 
   const [name, setName] = useState(data.name);
+  const [objective, setObjective] = useState("OBJECTIVE");
+  const [tec, setTec] = useState("TECHNICAL SKILLS");
+  const [soft, setSoft] = useState("SOFT SKILLS");
+  const [contact, setContact] = useState("CONTACT");
 
   return (
     <div className="resume">
-      <h1 style={{ alignItems: "center" }}>RESUME1</h1>
       <div className="name">
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <textarea
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            style={{
+              marginTop: "20px",
+              textAlign: "center",
+              fontSize: "40px",
+              fontWeight: "900",
+              width: "100%",
+              backgroundColor: "transparent",
+              border: "0px solid transparent",
+              resize: "none",
+              overflow: "hidden",
+            }}
+            onInput={(e) => {
+              const textarea = e.target as HTMLTextAreaElement;
+              textarea.style.height = "auto";
+              textarea.style.height = `${textarea.scrollHeight}px`;
+            }}
+          />
+        </div>
         <input
           type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={objective}
+          onChange={(e) => setObjective(e.target.value)}
           style={{
-            textAlign: "center",
-            fontSize: "24px",
-            fontWeight: "bold",
+            marginTop: "10px",
             width: "100%",
-            marginBottom: "10px",
+            fontSize: "25px",
+            fontWeight: "bold",
+            backgroundColor: "transparent",
+            border: "0px solid transparent",
+            borderRadius: "5px",
+            color: "#1995f5",
+            resize: "none",
+            overflow: "hidden",
+          }}
+          onInput={(e) => {
+            const textarea = e.target as HTMLTextAreaElement;
+            textarea.style.height = "auto";
+            textarea.style.height = `${textarea.scrollHeight}px`;
           }}
         />
-        <h2>OBJECTIVE</h2>
         <p>{oneLineDescription}</p>
 
         <section>
-          <h2>TECHNICAL SKILLS</h2>
+          <input
+            type="text"
+            value={tec}
+            onChange={(e) => setTec(e.target.value)}
+            style={{
+              width: "100%",
+              fontSize: "25px",
+              fontWeight: "bold",
+              backgroundColor: "transparent",
+              border: "0px solid transparent",
+              borderRadius: "5px",
+              color: "#1995f5",
+              resize: "none",
+              overflow: "hidden",
+            }}
+            onInput={(e) => {
+              const textarea = e.target as HTMLTextAreaElement;
+              textarea.style.height = "auto";
+              textarea.style.height = `${textarea.scrollHeight}px`;
+            }}
+          />
           <ul>
             {skillset.map(({ title, proficiency }, index) => (
               <li key={index}>
@@ -45,7 +106,27 @@ const Resume: React.FC<{ data: ResumeData }> = ({ data }) => {
         </section>
 
         <section>
-          <h2>SOFT SKILLS</h2>
+          <input
+            type="text"
+            value={soft}
+            onChange={(e) => setSoft(e.target.value)}
+            style={{
+              width: "100%",
+              fontSize: "25px",
+              fontWeight: "bold",
+              backgroundColor: "transparent",
+              border: "0px solid transparent",
+              borderRadius: "5px",
+              color: "#1995f5",
+              resize: "none",
+              overflow: "hidden",
+            }}
+            onInput={(e) => {
+              const textarea = e.target as HTMLTextAreaElement;
+              textarea.style.height = "auto";
+              textarea.style.height = `${textarea.scrollHeight}px`;
+            }}
+          />
           <ul>
             {softskills.map(({ title }, index) => (
               <li key={index}>{title}</li>
@@ -53,7 +134,27 @@ const Resume: React.FC<{ data: ResumeData }> = ({ data }) => {
           </ul>
         </section>
 
-        <h2>CONTACT</h2>
+        <input
+          type="text"
+          value={contact}
+          onChange={(e) => setContact(e.target.value)}
+          style={{
+            width: "100%",
+            fontSize: "25px",
+            fontWeight: "bold",
+            backgroundColor: "transparent",
+            border: "0px solid transparent",
+            borderRadius: "5px",
+            color: "#1995f5",
+            resize: "none",
+            overflow: "hidden",
+          }}
+          onInput={(e) => {
+            const textarea = e.target as HTMLTextAreaElement;
+            textarea.style.height = "auto";
+            textarea.style.height = `${textarea.scrollHeight}px`;
+          }}
+        />
         <p>Phone: {contactDetails.phone}</p>
         <p>Address: {contactDetails.address}</p>
         <p>Email: {contactDetails.email}</p>
